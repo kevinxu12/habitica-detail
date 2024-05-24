db86fb81e84c3dc76a3907c878b9a084c4f9ec82
Kevin Xu <40180478+kevinxu12@users.noreply.github.com>
2024-05-24 15:12:25 -0700

    Change some jest configs

---
+++ jest.detail.js
@@ -0,0 +1,17 @@
+module.exports = {
+    testEnvironment: "<rootDir>/website/detail/generated/detail.environment.ts",
+    setupFilesAfterEnv: ["<rootDir>/website/detail/generated/detail.setup.ts"],
+    transform: {
+      "^.+\\.[tj]sx?$": [
+        "ts-jest",
+        {
+          isolatedModules: true,
+        },
+      ],
+    },
+    moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+    testTimeout: 20000, // 20 seconds
+    collectCoverage: true,
+    coverageDirectory: "<rootDir>/coverage/detail",
+    coverageReporters: ["lcov"],
+  };
