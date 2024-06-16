5e39cefca05e6fade1199868c726ae3b68eab666
Detail <dan+gh@detail.dev>
2024-06-16 16:29:09 -0700

    Update test/common/ops/disableClasses.js

--- test/common/ops/disableClasses.js
+++ test/common/ops/disableClasses.js
@@ -1,7 +1,6 @@
 import disableClasses from '../../../website/common/script/ops/disableClasses';
-import {
-  generateUser,
-} from '../../helpers/common.helper';
+import { generateUser } from '../../helpers/common.helper';
+import { MAX_LEVEL } from '../../../website/common/script/constants';
 
 describe('shared.ops.disableClasses', () => {
   let user;
@@ -30,6 +29,33 @@ describe('shared.ops.disableClasses', () => {
     expect(user.preferences.disableClasses).to.equal(true);
     expect(user.preferences.autoAllocate).to.equal(true);
     expect(user.stats.str).to.equal(34);
+    expect(user.stats.points).to.equal(0);
+  });
+
+  it('disables classes and allocates points correctly when level exceeds max', () => {
+    const maxLevelExceeded = MAX_LEVEL + 5;
+    
+    user.stats.lvl = maxLevelExceeded;
+    user.stats.str = 25;
+    user.stats.class = 'rogue';
+    user.flags.classSelected = false;
+    user.preferences.disableClasses = false;
+    user.preferences.autoAllocate = false;
+    user.stats.points = 10;
+
+    const [data] = disableClasses(user);
+    expect(data).to.eql({
+      stats: user.stats,
+      flags: user.flags,
+      preferences: user.preferences,
+    });
+
+    expect(user.stats.lvl).to.equal(maxLevelExceeded);
+    expect(user.stats.class).to.equal('warrior');
+    expect(user.stats.str).to.equal(MAX_LEVEL);
+    expect(user.flags.classSelected).to.equal(true);
+    expect(user.preferences.disableClasses).to.equal(true);
+    expect(user.preferences.autoAllocate).to.equal(true);
     expect(user.stats.points).to.equal(0);
   });
 });
